57363f5e0a57f91f92bb9d9991b5bafb0f16ee39
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-05 12:14:33 +0300

    Задача на большее число из двух

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт большее число.*/
+
+Console.WriteLine ("Введите первое число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1>num2) Console.WriteLine ("Из двух введенных чисел большее число " + num1);
+else Console.WriteLine ("Из двух введенных чисел большее число " + num2);
