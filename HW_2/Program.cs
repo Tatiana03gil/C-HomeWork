12b0a8f16bbc0aa70b74974a08d4f25030addd7f
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-08 11:08:57 +0300

    Задача на вторую цифру трехзначного числа

---
+++ HW_2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 10: Напишите программу, которая принимает
+на вход трёхзначное число и на выходе показывает вторую 
+цифру этого числа.*/
+
+int Dozen (int number){
+    int Dozens = number % 100 / 10;
+    return Dozens;
+        }
+
+Console.WriteLine ("Введите трехзначное число: ");
+int num = Convert.ToInt32 (Console.ReadLine ());
+int Dec = Dozen (num);
+Console.WriteLine ("Вторая цифра числа: " + Dec);
+
