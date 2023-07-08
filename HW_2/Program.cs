d81bc59b4f554227874ec44c1b568a543507af72
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-08 12:37:49 +0300

    Задача на дни недели

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -16,7 +16,7 @@ Console.WriteLine ("Вторая цифра числа: " + Dec);
 /*Задача 13: Напишите программу, которая выводит
  третью цифру заданного числа.*/
 
- int Third (int number){
+ /*int Third (int number){
     if (number >=100){
         while (number >=1000) number = number / 10;
         int Thirds = number % 10;
@@ -29,6 +29,18 @@ Console.WriteLine ("Вторая цифра числа: " + Dec);
     int num3 = Third (num);
     if (num3>-1) Console.WriteLine ("Третья цифра числа: " + num3);
     else Console.WriteLine ("У числа нет третьей цифры!");
+    */
 
+/*Задача 15: Напишите программу, которая принимает на вход
+цифру, обозначающую день недели, и проверяет, является
+ли этот день выходным.*/
 
+bool Holiday (int day){
+    if (day == 6 || day == 7 ) return true;
+    else return false;
+}
 
+Console.WriteLine ("Введите день недели:");
+int dayNum = Convert.ToInt32 (Console.ReadLine ());
+bool result = Holiday (dayNum);
+Console.WriteLine ("Это выходной день? " + result);
