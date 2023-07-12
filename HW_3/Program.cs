5578b90c6aaf837997b4d9518ec93279afdf5ef4
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-12 16:46:31 +0300

    Задача на расстояние между 2-мя точками в 3D

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -55,3 +55,28 @@ int num = Convert.ToInt32 (Console.ReadLine());
 bool result = Palindrom (num);
 Console.WriteLine (result);*/
 
+/*Задача 21
+Напишите программу, которая принимает на вход координаты двух 
+точек и находит расстояние между ними в 3D пространстве.*/
+
+double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distance = Math.Sqrt ((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+    return distance;
+}
+
+Console.WriteLine (" Введите координаты первой точки (X1, Y1, Z1):");
+int x1 = Convert.ToInt32 (Console.ReadLine ());
+int y1 = Convert.ToInt32 (Console.ReadLine ());
+int z1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine (" Введите координаты второй точки (X2, Y2, Z2):");
+int x2 = Convert.ToInt32 (Console.ReadLine ());
+int y2 = Convert.ToInt32 (Console.ReadLine ());
+int z2 = Convert.ToInt32 (Console.ReadLine ());
+
+double rasst = Distance (x1, y1, z1, x2, y2, z2);
+Console.Write ("Расстояние: " + rasst);
+
+
+
+
