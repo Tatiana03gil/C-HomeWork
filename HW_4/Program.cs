59fd66440d96c2c0a806554f02d6397a9b5c0139
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-15 11:17:32 +0300

    Возведение в степень

---
+++ HW_4/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 25: Напишите цикл, который принимает на
+ вход два числа (A и B) и возводит число A в 
+ натуральную степень B.*/
+
+ int RaiseToPower (int number, int power)
+ {
+    int result = 1;
+    for (int i = 1; i <= power; i++)
+        result = result*number;
+    return result;
+ }
+
+Console. WriteLine ("Введите число А: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console. WriteLine ("Введите число B: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+int result = RaiseToPower (a, b);
+Console. WriteLine ($"Число {a} в степени {b} равно {result}");
